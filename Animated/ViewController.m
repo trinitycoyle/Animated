652bb3baff454704999f05a4cb0b323dba323b35
Trinity <trinitycoyle@yahoo.com>
2016-12-24 10:55:13 -0500

    Objective-C Game Test
    Example game app

--- Animated/ViewController.m
+++ Animated/ViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSArray *imageNames = @[@"Cat.png",@"Dog.png",@"Bunny.png",@"Snail.png",@"Bird.png",@"Paw.png",@"Fish.png",@"Bone.png"];
+
+    NSMutableArray *images = [[NSMutableArray alloc] init];
+    for (int i = 0; i < imageNames.count; i++){
+        [images addObject:[UIImage imageNamed:[imageNames objectAtIndex:i]]];
+    }
+    UIImageView *animationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 200, 300, 300)];
+    animationImageView.animationImages = images;
+    animationImageView.animationDuration = 2;
+
+    [self.view addSubview:animationImageView];
+    [animationImageView startAnimating];
 }
 
 - (void)didReceiveMemoryWarning {
